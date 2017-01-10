6d2da32c2dff2c567db36fc42ab9f0f44f6de09c
Erozone <Erozone@Mohits-Mac-mini.local>
2017-01-10 22:27:24 +0530

    Initial Commit

---
+++ Databases/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Databases
+//
+//  Created by Mohit Kumar on 10/01/17.
+//  Copyright © 2017 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
